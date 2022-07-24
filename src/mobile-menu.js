37216b7732f68cf0e41b4a640fcc5e39d5218230
Olga Birkovska <birkovskaya92@ukr.net>
2022-07-24 19:59:31 +0300

    Добавляет корректное позиционирование мобильного меню для таблетки и альбомной ориентации моб версии

--- src/mobile-menu.js
+++ src/mobile-menu.js
@@ -4,13 +4,15 @@
     closeModalBtn: document.querySelector("[data-menu-close]"),
     modal: document.querySelector("[data-menu]"),
     bodyNoScroll: document.querySelector("[data-nonScroll]"),
+    containerGoWide: document.querySelector("[data-go-wide]"),
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
-      refs.modal.classList.toggle("is-open");
-      refs.bodyNoScroll.classList.toggle("no-scroll");
+    refs.modal.classList.toggle("is-open");
+    refs.bodyNoScroll.classList.toggle("no-scroll");
+    refs.containerGoWide.classList.toggle("go-wide");
   }
 })();
