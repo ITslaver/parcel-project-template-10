4dc18516e935ce334f0be79163e5e0c41dc4bca9
Viacheslav Polishchuk <mr_v.p@ukr.net>
2022-07-26 13:39:08 +0300

    fix js products

--- src/mobile-menu.js
+++ src/mobile-menu.js
@@ -21,32 +21,5 @@
   }
 })();
 
-// -------------------------------button read more-------------------------------------------------------
 
-function readMore() {
-  let more = document.querySelector('.more');
-  if (more.style.display === 'none') {
-    more.style.display = 'block';
-  } else {
-    more.style.display = 'block';
-    more.style.display = 'none';
-  }
-}
-function readMoreThird() {
-  let moreThird = document.querySelector('.more-third');
-  if (moreThird.style.display === 'none') {
-    moreThird.style.display = 'block';
-  } else {
-    moreThird.style.display = 'block';
-    moreThird.style.display = 'none';
-  }
-}
-function readMoreSecond() {
-  let moreSecond = document.querySelector('.more-second');
-  if (moreSecond.style.display === 'none') {
-    moreSecond.style.display = 'block';
-  } else {
-    moreSecond.style.display = 'block';
-    moreSecond.style.display = 'none';
-  }
-}
+
