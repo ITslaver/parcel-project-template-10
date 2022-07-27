9a387cbab2e33d6396fa59994022a8c775c32ee4
ITslaver <105318545+ITslaver@users.noreply.github.com>
2022-07-27 09:30:37 +0000

    Deploying to gh-pages from @ ITslaver/parcel-project-template-10@2de3c1a4caa0441c550f84eadb471c203ca23154 🚀

--- index.725a92ac.js
+++
@@ -1,2 +0,0 @@
-var time=2;cc=1,$(window).scroll((function(){$("#counter").each((function(){$(this).offset().top<$(window).scrollTop()+300&&cc<2&&$("div").each((function(){var t=1,c=$(this).data("num"),n=1e3*time/c,i=$(this),o=setInterval((function(){t<=c?i.html(t):(cc+=2,clearInterval(o)),t++}),n)}))}))}));
-//# sourceMappingURL=index.725a92ac.js.map
